2ef5b73cae00748fda8f7ee9d9ab931f05822fd5
RussianGravy <valentinejones1215@gmail.com>
2024-10-18 23:30:25 -0700

    added routes

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotePage from "./pages/NotePage";
+import SignUpPage from "./pages/SignUpPage";
+import { AuthProvider } from "./contexts/AuthContext";
+import SignInPage from "./pages/SignInPage";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" Component={HomePage} />
+          <Route path="/note" Component={NotePage} />
+          <Route path="/signup" Component={SignUpPage} />
+          <Route path="/login" Component={SignInPage} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
