e84b7eae7f84966f4cf908d2b3db1eea32ffb72f
mango-ship-it <christiansmith12321@gmail.com>
2024-10-19 15:17:45 -0700

    study buttons reworked

--- react-front/src/components/FuncBar.jsx
+++ react-front/src/components/FuncBar.jsx
@@ -5,10 +5,10 @@ export default function FuncBar() {
     <div className="w-full py-1 border-t-2 flex">
       <div></div> {/* for page linking laters */}
       <div className="mx-auto">
-        <button className="min-w-32 px-1 mx-1 bg-blue-600 text-white rounded-sm">
+        <button className="transition ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-cyan-400 duration-300 my-auto px-2 rounded-md bg-blue-500 text-white mx-auto drop-shadow-lg">
           Optimize
         </button>
-        <button className="min-w-32 px-1 mx-1 bg-blue-600 text-white rounded-sm">
+        <button className="transition ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-cyan-400 duration-300 my-auto px-2 rounded-md bg-blue-500 text-white mx-auto ml-2 drop-shadow-lg">
           Study
         </button>
       </div>
