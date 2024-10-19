a3e65fcba9ad180038b240d82d11b45700d9c5c6
RussianGravy <valentinejones1215@gmail.com>
2024-10-19 12:17:46 -0700

    implemented tinyDocs to update firebase

--- react-front/src/pages/NotePage.jsx
+++ react-front/src/pages/NotePage.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Navbar from "../components/Navbar";
 import { useParams } from "react-router-dom";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useAuth } from "../contexts/AuthContext";
+import { Editor } from "@tinymce/tinymce-react";
 
 export default function NotePage() {
   const [text, setText] = useState("");
   const authVariables = useAuth();
   const { id } = useParams();
   var textRef = doc(db, authVariables.currentUser.email, "notes", id, "text"); //reference to text doc
+
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
 
   async function getText() {
     var document = "";
@@ -52,16 +61,58 @@ export default function NotePage() {
   }, [text]);
 
   return (
-    <div className="w-screen h-screen">
-      {/* <div className="w-full h-1/6 bg-gray-300 fixed top-0"></div>
-      <div className="w-full h-1/6 bg-gray-300"></div> spacer */}
-      <textarea
-        value={text}
-        onChange={(e) => {
-          setText(e.target.value);
+    <>
+      <Navbar />
+      <Editor
+        apiKey="rncicr4pa0ungw5lzix98tz61buq6rodfdnx37txoh1hi0se"
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue={text}
+        init={{
+          setup: function (editor) {
+            editor.on("input", () => {
+              setText(editorRef.current.getContent());
+            });
+          },
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
-        className="w-full h-full p-10 text-wrap overflow-y-auto resize-none border-none"
       />
-    </div>
+      <button
+        className="p-4 text-white bg-blue-400 rounded-lg"
+        onClick={() => {
+          console.log();
+          setText(editorRef.current.getContent());
+        }}
+      >
+        Update Firebase
+      </button>
+    </>
   );
 }
