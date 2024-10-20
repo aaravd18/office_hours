42c443352e5f7d601abffba0bbf0f9b9578ffd28
RussianGravy <valentinejones1215@gmail.com>
2024-10-20 09:59:36 -0700

    recent bar

--- react-front/src/components/Searchbar.jsx
+++ react-front/src/components/Searchbar.jsx
@@ -5,14 +5,9 @@ export default function Searchbar() {
   function getResults(search) {
     if (search.length == 0) setResults([]);
     if (search.length > 0)
-      setResults([
-        "Fake Nonsense Class",
-        "These Are Made Up Notes",
-        "Never Happened Lecture",
-      ]);
-    if (search.length > 5)
-      setResults(["Fake Nonsense Class", "These Are Made Up Notes"]);
-    if (search.length > 10) setResults(["These Are Made Up Notes"]);
+      setResults(["Cellular Biology", "Bioengineering", "Bio Human Anatomy"]);
+    if (search.length > 4) setResults(["Cellular Biology", "Bioengineering"]);
+    if (search.length > 7) setResults(["Bioengineering"]);
   }
   return (
     <div className="w-max mr-5 mx-auto flex flex-col relative">
@@ -23,10 +18,10 @@ export default function Searchbar() {
         }}
         className="text-center indent-3 mx-auto my-auto px-15 border-4 border-blue-300 hover:border-blue-500 hover: duration-200 rounded-full drop-shadow-lg"
       />
-      <div className="w-full absolute top-14 text-xl text-center">
+      <div className="w-full absolute top-14 flex flex-col text-lg text-center">
         {results.map((result) => {
           return (
-            <a className="mx-auto" href={"note/" + result}>
+            <a className="mx-auto text-nowrap" href={"note/" + result}>
               {result}
             </a>
           );
